03afd873fa5a47861d6372207b557899c4978d4a
Dion Novalino <dionovalino@gmail.com>
2023-01-08 11:01:33 +0700

    update

--- src/components/detail-tour.js
+++ src/components/detail-tour.js
@@ -24,7 +24,7 @@ const Detail = () => {
         //change this to the script source you want to load, for example this is snap.js sandbox env
         const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
         //change this according to your client-key
-        const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
+        const myMidtransClientKey = "SB-Mid-client-hm0XAws-G5P8gy3-";
 
         let scriptTag = document.createElement("script");
         scriptTag.src = midtransScriptUrl;
